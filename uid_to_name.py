02d00097756e816a7aaf599731eac2b0e2de1c7e
Chandler Abraham <cabraham@twitter.com>
2014-11-10 00:14:12 -0800

    comments

--- uid_to_name.py
+++ uid_to_name.py
@@ -5,7 +5,7 @@ import sys
 uid_to_name = {}
 
 for line in sys.stdin:
-  # matches a line found in the lightroom catalog binary that looks like this
+  # matches a line found in the lightroom catalog binary that looks like this:
   # 9DD10023-1313-41EB-8BCE-0C3E166CFA2Amy_photo_namejpgA
   # which is the UID followed by a filename with no period followed by an A
   m = re.match(
@@ -13,8 +13,7 @@ for line in sys.stdin:
         line,
         re.IGNORECASE)
   if m:
-      # print('Match found: ', m.group(1), m.group(2), m.group(3))
-      # ('Match found: ', 'BF98706D-971C-41DA-AE1C-A86391C9AD1C', 'DSC_0831', 'NEF')
+      # ('BF98706D-971C-41DA-AE1C-A86391C9AD1C', 'DSC_0831', 'NEF')
       uid_to_name[m.group(1)] = m.group(2) + '.' + m.group(3)
 
 pickle.dump(uid_to_name, open('/tmp/uid_to_name.pickle','wb'))
