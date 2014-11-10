bf98b7d36c123093e3ae746f1f6cb2c50a37a8be
Chandler Abraham <cabraham@twitter.com>
2014-11-10 00:13:30 -0800

    indent

--- uid_to_name.py
+++ uid_to_name.py
@@ -11,12 +11,10 @@ for line in sys.stdin:
   m = re.match(
         '^([A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12})(.+)(jpg|jpeg|tif|nef|gif)A$',
         line,
-        re.IGNORECASE
-      )
+        re.IGNORECASE)
   if m:
       # print('Match found: ', m.group(1), m.group(2), m.group(3))
       # ('Match found: ', 'BF98706D-971C-41DA-AE1C-A86391C9AD1C', 'DSC_0831', 'NEF')
       uid_to_name[m.group(1)] = m.group(2) + '.' + m.group(3)
 
-f = open('/tmp/uid_to_name.pickle','wb')
-pickle.dump(uid_to_name, f)
+pickle.dump(uid_to_name, open('/tmp/uid_to_name.pickle','wb'))
